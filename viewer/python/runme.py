418caf77df2e9f1cb48867e48dbc80880db31ac1
Reza Zahiri <reza.zahiri@clarius.me>
2019-11-20 21:08:07 -0800

    Now importing the data reader to avoid duplication.

--- viewer/python/runme.py
+++ viewer/python/runme.py
@@ -12,74 +12,16 @@ Author: Reza Zahiri
 import numpy as np
 from scipy.signal import hilbert
 import matplotlib.pyplot as plt
-
-# read rf data
-def read_rf(filename):
-    hdr_info = ('id', 'frames', 'lines', 'samples', 'samplesize')
-    hdr, timestamps, data = {}, None, None
-    with open(filename, 'rb') as raw_bytes:
-        # read 4 bytes header 
-        for info in hdr_info:
-            hdr[info] = int.from_bytes(raw_bytes.read(4), byteorder='little')
-        # read timestamps and data
-        timestamps = np.zeros(hdr['frames'], dtype='int64')
-        sz = hdr['lines'] * hdr['samples'] * hdr['samplesize']
-        data = np.zeros((hdr['lines'], hdr['samples'], hdr['frames']), dtype='int16')
-        for frame in range(hdr['frames']):
-            # read 8 bytes of timestamp
-            timestamps[frame] = int.from_bytes(raw_bytes.read(8), byteorder='little')
-            # read each frame
-            data[:, :, frame] = np.frombuffer(raw_bytes.read(sz), dtype='int16').reshape([hdr['lines'], hdr['samples']])
-    print('Loaded {d[2]} raw frames of size, {d[0]} x {d[1]} (lines x samples)'.format(d=data.shape))
-    return hdr, timestamps, data
-
-# read iq data
-def read_iq(filename):
-    hdr_info = ('id', 'frames', 'lines', 'samples', 'samplesize')
-    hdr, timestamps, data = {}, None, None
-    with open(filename, 'rb') as raw_bytes:
-        # read 4 bytes header 
-        for info in hdr_info:
-            hdr[info] = int.from_bytes(raw_bytes.read(4), byteorder='little')
-        # read timestamps and data
-        timestamps = np.zeros(hdr['frames'], dtype='int64')
-        sz = hdr['lines'] * hdr['samples'] * hdr['samplesize']
-        data = np.zeros((hdr['lines'], hdr['samples'] * 2, hdr['frames']), dtype='int16')
-        for frame in range(hdr['frames']):
-            # read 8 bytes of timestamp
-            timestamps[frame] = int.from_bytes(raw_bytes.read(8), byteorder='little')
-            # read each frame
-            data[:, :, frame] = np.frombuffer(raw_bytes.read(sz), dtype='int16').reshape([hdr['lines'], hdr['samples']*2])
-    print('Loaded {d[2]} raw frames of size, {d[0]} x {d[1]} (lines x samples)'.format(d=data.shape))
-    return hdr, timestamps, data
-
-# read env data
-def read_env(filename):
-    hdr_info = ('id', 'frames', 'lines', 'samples', 'samplesize')
-    hdr, timestamps, data = {}, None, None
-    with open(filename, 'rb') as raw_bytes:
-        # read 4 bytes header 
-        for info in hdr_info:
-            hdr[info] = int.from_bytes(raw_bytes.read(4), byteorder='little')
-        # read timestamps and data
-        timestamps = np.zeros(hdr['frames'], dtype='int64')
-        sz = hdr['lines'] * hdr['samples'] * hdr['samplesize']
-        data = np.zeros((hdr['lines'], hdr['samples'], hdr['frames']), dtype='uint8')
-        for frame in range(hdr['frames']):
-            # read 8 bytes of timestamp
-            timestamps[frame] = int.from_bytes(raw_bytes.read(8), byteorder='little')
-            # read each frame
-            data[:, :, frame] = np.frombuffer(raw_bytes.read(sz), dtype='uint8').reshape([hdr['lines'], hdr['samples']])
-    print('Loaded {d[2]} raw frames of size, {d[0]} x {d[1]} (lines x samples)'.format(d=data.shape))
-    return hdr, timestamps, data
-
+import sys
+sys.path.append("../../common/python")
+import rdataread as rd
 
 if __name__ == '__main__':
     
     # read rf data
     path = "../data/wirephantom/"
     filename = "phantom_rf.raw"
-    hdr, timestamps, data = read_rf(path + filename)
+    hdr, timestamps, data = rd.read_rf(path + filename)
     
     # covnert to B 
     numframes = hdr['frames']
@@ -101,7 +43,7 @@ if __name__ == '__main__':
     # read iq data
     path = "../data/wirephantom/"
     filename = "phantom_iq.raw"
-    hdr, timestamps, data = read_iq(path + filename)
+    hdr, timestamps, data = rd.read_iq(path + filename)
     # separating i and q data
     idata = data[:,0::2,:]
     qdata = data[:,1::2,:]    
@@ -129,7 +71,7 @@ if __name__ == '__main__':
     # read b/envelope data
     path = "../data/wirephantom/"
     filename = "phantom_env.raw"
-    hdr, timestamps, data = read_env(path + filename)
+    hdr, timestamps, data = rd.read_env(path + filename)
 
     # display b data
     numframes = hdr['frames']
